e2136fda777cf68bda56ef8ddbeb927bef0981c4
win7 <ssertaceker@hotmail.com>
2017-01-03 17:20:25 +0300

    login and logout finished.

--- src/WebApplication1/Controllers/AccountController.cs
+++ src/WebApplication1/Controllers/AccountController.cs
@@ -56,5 +56,36 @@ namespace CachePostRest.Controllers
 
             return result.Succeeded;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"> username or email</param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<bool> Login(string username, string password)
+        {
+            var user = await userManager.FindByNameAsync(username) ?? await userManager.FindByEmailAsync(username);
+
+            if (user != null)
+            {
+                var result = await loginManager.PasswordSignInAsync(user, password, true, false);
+                return result.Succeeded;
+            }
+            else //kullanici yok
+                return false;
+        }
+
+        public async Task<bool> Logout()
+        {
+            if (User != null)
+            {
+                await loginManager.SignOutAsync();
+                return true;
+            }
+            else // giris yapmis kullanici bulunamadi
+                return false;
+        }
     }
 }
